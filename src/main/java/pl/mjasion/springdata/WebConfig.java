642dd5251d37d8b9bae59b7852fd1cfcd2c034b3
Marcin Jasion <marcin.jasion@gmail.com>
2014-01-01 21:08:40 +0100

    empty project initialized

---
+++ src/main/java/pl/mjasion/springdata/WebConfig.java
@@ -0,0 +1,8 @@
+package pl.mjasion.springdata;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+public class WebConfig extends RepositoryRestMvcConfiguration {
+}
